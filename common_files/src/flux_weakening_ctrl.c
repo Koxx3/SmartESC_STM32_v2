a5239ed49074c6a29b01495410b54499fd9db830
Christopher Breton-Jean <cbretonjean@hotmail.com>
2021-11-20 05:53:38 -0500

    **** WARNING *** MUST BE VALIDATED Field Weakening

--- common_files/src/flux_weakening_ctrl.c
+++ common_files/src/flux_weakening_ctrl.c
@@ -41,10 +41,8 @@ __weak void FW_Clear( FW_Handle_t * pHandle )
   */
 __weak void FW_DataProcess( FW_Handle_t * pHandle, qd_t Vqd )
 {
-	  int16_t v3 = Vqd.d + (pHandle->hVqdLowPassFilterBW - 1) * pHandle->AvVolt_qd.d;
-	  int16_t v4 = Vqd.q + (pHandle->hVqdLowPassFilterBW - 1) * pHandle->AvVolt_qd.q;
-	  pHandle->AvVolt_qd.q = v4 >> pHandle->hVqdLowPassFilterBWLOG;
-	  pHandle->AvVolt_qd.d = v3 >> pHandle->hVqdLowPassFilterBWLOG;
+	  pHandle->AvVolt_qd.q = (Vqd.q + (pHandle->hVqdLowPassFilterBW - 1) * pHandle->AvVolt_qd.q) >> pHandle->hVqdLowPassFilterBWLOG;
+	  pHandle->AvVolt_qd.d = (Vqd.d + (pHandle->hVqdLowPassFilterBW - 1) * pHandle->AvVolt_qd.d) >> pHandle->hVqdLowPassFilterBWLOG;
 }
 
 /**
@@ -62,39 +60,40 @@ __weak void FW_DataProcess( FW_Handle_t * pHandle, qd_t Vqd )
 __weak qd_t FW_CalcCurrRef( FW_Handle_t * pHandle, qd_t Iqdref ){
 	  int16_t AvVoltAmpl = MCM_Sqrt(pHandle->AvVolt_qd.d * pHandle->AvVolt_qd.d + pHandle->AvVolt_qd.q * pHandle->AvVolt_qd.q);
 
-	  int16_t v8;
-	  if (AvVoltAmpl > 0x7FFF )
-		   v8 = ((pHandle->hMaxModule * pHandle->hFW_V_Ref) / 0x3E8) - 0x7FFF;
-	  else
-	 	   v8 = ((pHandle->hMaxModule * pHandle->hFW_V_Ref) / 0x3E8) - AvVoltAmpl;
+	  int16_t varError;
+	  if (AvVoltAmpl > 0x7FFF ){
+		  varError = ((pHandle->hMaxModule * pHandle->hFW_V_Ref) / 0x3E8) - 0x7FFF;
+	  } else {
+		  varError = ((pHandle->hMaxModule * pHandle->hFW_V_Ref) / 0x3E8) - AvVoltAmpl;
+	  }
 
 	  pHandle->AvVoltAmpl = AvVoltAmpl;
-	  int16_t v9 = PI_Controller(pHandle->pFluxWeakeningPID, v8);
+	  int16_t pid_output = PI_Controller(pHandle->pFluxWeakeningPID, varError);
 
-	  if ( v9 < 0 ){
+	  if (pid_output < 0){
 		  pHandle->AvVolt_qd.d = pHandle->hIdRefOffset;
 	  }
 
-	  if ( v9 >= 0 ){
+	  if (pid_output >= 0){
 		  pHandle->hIdRefOffset = pHandle->AvVolt_qd.d;
 	  } else {
-		  pHandle->AvVolt_qd.d += v9;
+		  pHandle->AvVolt_qd.d += pid_output;
 	  }
 
 	  if (pHandle->hDemagCurrent < pHandle->AvVolt_qd.d){
 		  pHandle->hDemagCurrent = pHandle->AvVolt_qd.d;
 	  }
 
-	  uint16_t v11 = pHandle->hDemagCurrent;
-	  int v12 = MCM_Sqrt(pHandle->wNominalSqCurr - pHandle->hDemagCurrent * pHandle->hDemagCurrent);
-	  int v13 = v12 * PID_GetKIDivisor(pHandle->pSpeedPID);
+	  uint16_t demCurr = pHandle->hDemagCurrent;
+	  int sqRoot = MCM_Sqrt(pHandle->wNominalSqCurr - pHandle->hDemagCurrent * pHandle->hDemagCurrent);
+	  int lowerLimit = sqRoot * PID_GetKIDivisor(pHandle->pSpeedPID);
 
-	  PID_SetLowerIntegralTermLimit(pHandle->pSpeedPID, -v13);
-	  PID_SetUpperIntegralTermLimit(pHandle->pSpeedPID, v13);
+	  PID_SetLowerIntegralTermLimit(pHandle->pSpeedPID, -lowerLimit);
+	  PID_SetUpperIntegralTermLimit(pHandle->pSpeedPID, lowerLimit);
 
-	  qd_t output = {(int16_t)pHandle->AvVolt_qd.q, (int16_t)(v11 << 16)};
-	  if ( pHandle->AvVolt_qd.q > v12 || (v12 = -v12, pHandle->AvVolt_qd.q < v12) ){
-		  output.q = v12;
+	  qd_t output = {(int16_t)pHandle->AvVolt_qd.q, (int16_t)(demCurr << 16)};
+	  if ( pHandle->AvVolt_qd.q > sqRoot || (sqRoot = -sqRoot, pHandle->AvVolt_qd.q < sqRoot) ){
+		  output.q = sqRoot;
 	  }
 
 	  //return (unsigned __int16)v5 | (v11 << 16);
