9b149bd87d094f5fd49c8073c7aff6136e10c83b
Netzpfuscher <jens.kerrinnes@gmail.com>
2021-12-27 21:17:26 +0100

    terminal foc_openloop implemented

--- common_files/src/VescToSTM.c
+++ common_files/src/VescToSTM.c
@@ -111,6 +111,19 @@ void VescToSTM_set_open_loop(bool enabled, int16_t init_angle, int16_t erpm){
 	}
 }
 
+void VescToSTM_ramp_current(float current){ //in Amps
+	current*=1000;
+	qd_t currComp;
+	currComp.q = 0;
+	currComp.d = 0;
+	for (int i = 0;i < 1000;i++) {
+		currComp.q = current_to_torque((float)i * current / 1000.0);
+		MCI_SetCurrentReferences(pMCI[M1],currComp);
+		vTaskDelay(MS_TO_TICKS(1));
+		VescToSTM_timeout_reset();
+	}
+}
+
 void VescToSTM_set_open_loop_rpm(int16_t erpm){
 	SpeednTorqCtrlM1.SPD->open_speed = erpm_to_int16(erpm);
 }
@@ -137,9 +150,11 @@ void VescToSTM_handle_timeout(){
 	if(!timeout_enable) {
 		VescToSTM_timeout_reset();
 	}
-	if((xTaskGetTickCount() - last_reset) > (appconf.timeout_msec*2)){
-		VescToSTM_set_brake(appconf.timeout_brake_current*1000);
-		last_reset = xTaskGetTickCount();
+	if(appconf.timeout_msec){
+		if((xTaskGetTickCount() - last_reset) > (appconf.timeout_msec*2)){
+			VescToSTM_set_brake(appconf.timeout_brake_current*1000);
+			last_reset = xTaskGetTickCount();
+		}
 	}
 };
 void VescToSTM_enable_timeout(bool enbale){
